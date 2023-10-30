9bcbd65fc7d7f27b1569b33ca7937caf2f8b4200
Raphael Lima <raphaeldesousalm@gmal.com>
2023-10-30 13:59:48 -0300

    fix: remove carregando

--- src/components/ProfileHero/index.tsx
+++ src/components/ProfileHero/index.tsx
@@ -160,7 +160,7 @@ const ProfileHero = ({ id, heroes }: Props) => {
           </div>
         </div>
       ) : (
-        <span className="text-lg">Carregando...</span>
+        false
       )}
     </div>
   );
